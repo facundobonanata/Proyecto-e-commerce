0fa14bb5eb825f934aae8833acf0cc0e39c40ff8
facundobonanata <gonzaloblmnt@gmail.com>
2021-09-19 14:16:47 -0300

    Actualizacón

--- js/product-info.js
+++ js/product-info.js
@@ -1,6 +1,88 @@
+let product = {};
+let comment = {};
+
+const showImagesGallery = (array) => {
+  let htmlContentToAppend = '';
+  for (let i = 0; i < array.length; i++) {
+    let imageSrc = array[i];
+
+    htmlContentToAppend += `
+    <div class="col-lg-3 col-md-4 col-6">
+      <div class="d-block mb-4 h-100">
+        <img class="img-fluid img-thumbnail" src="` + imageSrc + `" alt="">
+      </div>
+    </div>
+    `
+  }
+  document.getElementById('productsImagesGallery').innerHTML = htmlContentToAppend;
+}
+
+function showCommentsList(){
+
+  let htmlContentToAppend = "";
+  for(let i = 0; i < commentArray.length; i++){
+      let comment = commentArray[i];
+   var stars = `<span class="fa fa-star checked"></span>`
+       {
+           htmlContentToAppend += `
+           
+           <div class="">
+                  <div class="d-flex">                 
+                  <div class="starsRating">` + stars.repeat(comment.score) + `</p>
+                  </div>
+                  </div>
+                                          
+                <span class=""><strong>`+ comment.user +`</strong></span>
+              
+                          <p class="mb-1"><em>` + comment.description + ` </em></p>
+                      </div>
+                    </div>
+                    </div>
+                  <p class="mb-1">` + comment.dateTime + `</p>
+                  </div>
+              </div>
+              
+              <hr>
+              
+          `;
+      }
+
+      document.getElementById("comments").innerHTML = htmlContentToAppend;
+  }};
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+document.addEventListener("DOMContentLoaded", function (e) {
+    getJSONData(PRODUCT_INFO_URL).then((resObj) => {
+      if (resObj.status === 'ok') {
+         product = resObj.data;
+  
+        let productNameHTML = document.getElementById('productName');
+        let productDescriptionHTML = document.getElementById('productDescription');
+        let productCostHTML = document.getElementById('productCost');
+        let productCurrencyHTML = document.getElementById('productCurrency');
+        let productSoldCountHTML = document.getElementById('productSoldCount');
+        let productCategoryHTML = document.getElementById('productCategory');
+  
+        productNameHTML.innerHTML = product.name;
+        productDescriptionHTML.innerHTML = product.description;
+        productCostHTML.innerHTML = product.cost;
+        productCurrencyHTML.innerHTML = product.currency;
+        productSoldCountHTML.innerHTML = product.soldCount;
+        productCategoryHTML.innerHTML = product.category;
+  
+        showImagesGallery(product.images);
+      }
+    }); 
+    getJSONData(PRODUCT_INFO_COMMENTS_URL).then(function(resultObj){
+      if (resultObj.status === "ok")
+      {
+          commentArray = resultObj.data;            
 
+          showCommentsList();
+      }
+  });      
 });
+
+
--- js/products.js
+++ js/products.js
@@ -1,5 +1,5 @@
-const PRODUCTS_ASC = "AZ";
-const PRODUCTS_DES = "ZA";
+const PRODUCTS_ASC = "De menor a mayor";
+const PRODUCTS_DES = "De mayor a menor";
 const PRODUCTS_RELEVANCE = "Relevancia"
 let currentProductsArray = [] 
 let currentSortCriteria = undefined;
@@ -46,23 +46,23 @@ const showProductsList = () => {
             ((maxCost == undefined) || (maxCost != undefined && parseInt(products.soldCount) <= maxCost))){
 
             htmlContentToAppend += `
-		<div class="list-group-item list-group-item-action">
-                <div class="row">
-                    <div class="col-3">
-                        <img src="` + products.imgSrc + `" alt="` + products.description + `" class="img-thumbnail">
-                    </div>
-                    <div class="col">
-                        <div class="d-flex w-100 justify-content-between">
-                            <h4 class="mb-1">`+ products.name + `</h4>
-                            <small class="text-muted">` + products.soldCount + ` artículos.</small>
-                        </div>
-						<div>
-                    <p>`+ products.description + `</p>
-                    <p> A tan solo `+ products.currency + "   " + products.cost + `</p>
-                    </div>
-                    </div>
+		<a href= "product-info.html" class="list-group-item list-group-item-action">
+        <div class="row">
+        <div class="col-3">
+            <img src="` + products.imgSrc + `" alt="` + products.description + `" class="img-thumbnail">
+            </div>
+        <div class="col">
+        <div class="d-flex w-100 justify-content-between">
+            <h4 class="mb-1">`+ products.name + `</h4>
+                <small class="text-muted">` + products.soldCount + ` artículos vendidos.</small>
+            </div>
+		<div>
+            <p>`+ products.description + `</p>
+             <p> Precio `+ products.currency + "   " + products.cost + `</p>
+                     </div>
                 </div>
             </div>
+        </div>
             `
         }
         document.getElementById("pro-list-container").innerHTML = htmlContentToAppend;
@@ -95,22 +95,22 @@ document.addEventListener("DOMContentLoaded", function (e) {
     });
 
     //funcion que al click ordena de forma ascendnete
-    document.getElementById("sortAsc").addEventListener("click",()=>{
+    document.getElementById("sortAsc").addEventListener("click", function(){
         sortAndShowProducts(PRODUCTS_ASC);
     });
 
     // funcion que al click ordena de forma descendente
-    document.getElementById("sortDesc").addEventListener("click",()=>{
+    document.getElementById("sortDesc").addEventListener("click", function(){
         sortAndShowProducts(PRODUCTS_DES);
     });
 
     // funcion que al click ordena por relevancia
-    document.getElementById("sortBySoldCount").addEventListener("click",()=>{
+    document.getElementById("sortBySoldCount").addEventListener("click", function(){
         sortAndShowProducts(PRODUCTS_RELEVANCE);
     });
 
     // funcion que al click resetea el filtro de cantidad
-    document.getElementById("clearRangeFilter").addEventListener("click", ()=>{
+    document.getElementById("clearRangeFilter").addEventListener("click", function(){
         document.getElementById("rangeFilterCostMin").value = "";
         document.getElementById("rangeFilterCostMax").value = "";
 
@@ -119,11 +119,9 @@ document.addEventListener("DOMContentLoaded", function (e) {
 
         showProductsList();
     });
-
-
-    document.getElementById("rangeFilterCost").addEventListener("click",
-    ()=>{
-        //Obtengo el minimo y maximo de los intervalos para filtrar por cantidad de productos
+    
+    document.getElementById("rangeFilterCost").addEventListener("click", function(){
+        //Obtengo el minimo y maximo de los intervalos para filtrar por precio de productos
         minCost = document.getElementById("rangeFilterCostMin").value;
         maxCost = document.getElementById("rangeFilterCostMax").value;
 
@@ -144,4 +142,5 @@ document.addEventListener("DOMContentLoaded", function (e) {
 
         showProductsList();
     });
-});
+    
+ });
